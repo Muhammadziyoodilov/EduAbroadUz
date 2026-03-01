92409305544cc54f9953168ae08057a8eaf88178
Muhammadziyoodilov <odilovmuhammadziyo100@gmail.com>
2026-03-01 12:36:24 +0500

    Add Firebase hosting config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,23 @@
-declare module '*.module.scss' {
+declare module "*.module.scss" {
   const classes: { [key: string]: string };
   export default classes;
 }
 
-declare module '*.module.css' {
+declare module "*.module.css" {
   const classes: { [key: string]: string };
   export default classes;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+  readonly VITE_FIREBASE_MEASUREMENT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
 }
